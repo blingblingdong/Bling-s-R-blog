70f5c9e3aa51e40262d3b05cb9e6fdb02e41ea36
blingblingdong <whoareyouman0.4@gmail.com>
2023-10-14 15:45:23 +0800

    hhh

--- distill.R
+++ distill.R
@@ -2,3 +2,15 @@ create_post("Java的變數",collection ="Java課程")
 #中文讀不出來
 
 library(distill)
+
+git config --global user.name "Jane Doe"
+
+git config --global user.name "blingblingdong"
+git config --global user.email "whoareyouman0.4@gmail.com"
+git config --global --list
+
+library(usethis)
+use_git_config(user.name = "blingblingdong", user.email = "whoareyouman0.4@gmail.com")
+
+
+create_post("Java_Operater",collection ="Java課程")
